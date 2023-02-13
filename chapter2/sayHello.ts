eb0d4a99f5322620ebb746241fbd4d0229612701
KazumasaYasui <silver.soul39@gmail.com>
2023-02-13 23:18:27 +0900

    async/awaitの修正、型定義ファイルの修正

--- chapter2/sayHello.ts
+++ chapter2/sayHello.ts
@@ -696,24 +696,28 @@ if (typeof y === 'string') {
 
 // ---
 
-// {
-//   function fetchFromServer(id: string): Promise<{ success: boolean }> {
-//     return new Promise(resolve => {
-//       setTimeout(() => {
-//         resolve({success: true})
-//       }, 100)
-//     })
-//   }
-//
-//   async function asyncFunc(): Promise<string> {
-//     const result = await fetchFromServer('111')
-//     return `The result: ${result.success}`
-//   }
-//
-//   (async () => {
-//     const result = await asyncFunc()
-//     console.log(result)
-//   })()
-//
-//   asyncFunc().then(result => console.log(result))
-// }
+function fetchFromServer(id: string): Promise<{ success: boolean }> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({success: true})
+    }, 100)
+  })
+}
+
+async function asyncFunc(): Promise<string> {
+  const result = await fetchFromServer('111')
+  return `The result: ${result.success}`
+}
+
+(async () => {
+  const result = await asyncFunc()
+  console.log(result)
+})()
+
+asyncFunc().then(result => console.log(result))
+
+// ---
+
+import { hello } from './lib/hello'
+
+hello('Kazumasa')
