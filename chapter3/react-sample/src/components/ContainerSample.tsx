b1fc7f9c0cc6a00237f2e2646e033961c473924e
KazumasaYasui <silver.soul39@gmail.com>
2023-04-18 23:15:21 +0900

    ContainerPropsの型定義

--- chapter3/react-sample/src/components/ContainerSample.tsx
+++ chapter3/react-sample/src/components/ContainerSample.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React from 'react';
+
+// Containerコンポーネントに渡すpropsの型を定義
+type ContainerProps = {
+  title: string
+  children: React.ReactNode
+}
 
 // Containerコンポーネントは、子要素を赤背景色で囲むコンポーネント
-const Container = (props: { title: string; children: React.ReactElement }) => {
+const Container = (props: ContainerProps) => {
 const { title, children } = props
   return (
     <div style={{ background: 'red' }}>
       <span>{title}</span>
-      {/* props の children を埋め込むと、このコンポーネントの開始タグと閉じタグで囲んだ要素を表示する */}
+      {/* propsのchildrenを埋め込むと、このコンポーネントの開始タグと閉じタグで囲んだ要素を表示する */}
       <div>{children}</div>
     </div>
   )
